e86ef4ead27ff7ed49c3c2b6e44513157a2cc85f
ToPresa <topresa16@hotmail.com>
2015-05-21 21:16:46 +0100

    blah5

--- src/Robot/main.java
+++ src/Robot/main.java
@@ -1,4 +1,7 @@
 package Robot;
+import java.util.ArrayList;
+import java.util.List;
+
 import Parser.Reader;
 import Astar.Astar;
 import Astar.State;
@@ -6,7 +9,32 @@ public class main {
 	    public static void main(String[] args) {
 	        System.out.println("Hello, World!");
 	        Reader parser=new Reader();
-	        parser.readFile("input.txt");
+	        
+	        List<Object> allobject=parser.readFile("input.txt");
+	        
+	        Robot rob=null;
+	        Warehouse ware=null;
+	        List<Box> boxes = new ArrayList<Box>();
+	        List<Wall> obstacles = new ArrayList<Wall>();
+	        System.out.println(allobject);
+	        int size=allobject.size();
+	        for(int counter=0; counter<size;counter++){
+	        	Object what=allobject.get(counter);
+	        	System.out.println(what);
+	        	if(what instanceof Robot)
+	        		rob=(Robot) what;
+	        	else if(what instanceof Warehouse)
+	        		ware=(Warehouse) what;
+	        	else if(what instanceof Box)
+	        		boxes.add((Box) what);
+	        	else if(what instanceof Wall)
+	        		obstacles.add((Wall) what);
+	        	else{
+	        		System.out.println("Argument error");
+	        		System.exit(4);
+	        	}
+	        }
+	        
 	        
 	        State initial = new State (1,1);
 	        State target =  new State (3,3);
