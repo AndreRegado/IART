016f7d8e32dac27591a5c737698b88ffc5a84b38
Azmery <FranciscoSousaCouto@outlook.com>
2015-05-28 23:29:48 +0100

    Play that shit mofo

--- src/Gui/Interface.java
+++ src/Gui/Interface.java
@@ -27,14 +27,18 @@ public class Interface extends JFrame{
 	  JButton playB;
 	  JButton exitB;
 	  
-	  private Pane background; 
-	  private JLabel robot;
-	  private JLabel obst2;
-	  private JLabel box2;
-	  private JLabel war2;
-	  private JLabel rasto;
-	  /*private JLabel rob = new JLabel(new ImageIcon("images/robot.png"));
-	  private JPanel pane;*/
+	   Pane background; 
+	   JLabel robot;
+	   JLabel obst2;
+	   JLabel box2;
+	   JLabel war2;
+	   JLabel rasto;
+
+	   final ImageIcon rob;
+	   ImageIcon box;
+	   ImageIcon obst;
+	   ImageIcon war;
+	   ImageIcon rasto1;
 	  
 
 	    //Button handlers:
@@ -43,16 +47,26 @@ public class Interface extends JFrame{
 	    private ExitButtonHandler ebHandler;
 	    
         final List<Point> path = new ArrayList<Point>();
-	        Point p1 = new Point(1,0);
-	        Point p2 = new Point(2,1);
-	        Point p3 = new Point(3,1);
-	        Point p4 = new Point(2,4);
+	    Point p1 = new Point(1,0);
+	    Point p2 = new Point(2,1);
+	    Point p3 = new Point(3,1);
+	    Point p4 = new Point(2,4);
 	      
-	        int vezes=0;
+	    int vezes=0;
+	    
+	    final Robot ObjRobot;
+	    Warehouse ObjWare;
+	    List<Box> ObjBoxes;
+	    List<Point> ObjObstacles;
 	 
 	 public Interface(final Robot ObjRobot, Warehouse ObjWare, List<Box> ObjBoxes, List<Point> ObjObstacles)
 
 	    {	
+		 
+		 	this.ObjRobot = ObjRobot;
+		 	this.ObjWare = ObjWare;
+		 	this.ObjBoxes = ObjBoxes;
+		 	this.ObjObstacles = ObjObstacles;
 
 	        setTitle("Robot box catching Simulator!");
 	        setLayout(new BorderLayout());
@@ -84,226 +98,31 @@ public class Interface extends JFrame{
 	        background.add(playB);
 	        background.add(exitB);
 	        
-	        final ImageIcon rob = new ImageIcon("images/robot.png");
+	        rob = new ImageIcon("images/robot.png");
 			robot = new JLabel(rob);
-			ImageIcon box = new ImageIcon("images/box.png");
+			box = new ImageIcon("images/box.png");
 			box2 = new JLabel(box);
-			ImageIcon obst = new ImageIcon("images/obst.png");
+			obst = new ImageIcon("images/obst.png");
 			obst2 = new JLabel(obst);
-			ImageIcon war = new ImageIcon("images/warehouse.png");
+			war = new ImageIcon("images/warehouse.png");
 			war2 = new JLabel(war);
-			final ImageIcon rasto1 = new ImageIcon("images/rasto.png");
+			rasto1 = new ImageIcon("images/rasto.png");
 			rasto = new JLabel(rasto1);
-			
-			List<Integer> ArrayX = new ArrayList<Integer>();
-			List<Integer> ArrayY = new ArrayList<Integer>();
-			
-			ArrayX.add(ObjRobot.getX());
-			ArrayY.add(ObjRobot.getY());
-			ArrayX.add(ObjWare.getX());
-			ArrayY.add(ObjWare.getY());
-			
-			//System.out.println("ROBOT: " + ObjRobot.getX() + ObjRobot.getY() + " ARMAZZEM: " + ObjWare.getX()+ ObjWare.getY());
-			
-	       for(int i =0; i < ObjBoxes.size(); i++) {
-	    	   ArrayX.add(ObjBoxes.get(i).getX());
-	    	   ArrayY.add(ObjBoxes.get(i).getY());
-	    	   //System.out.println(ObjBoxes.get(i).getX() + ObjBoxes.get(i).getY());
-	       }
-	       
-	       int maiorX = 0, maiorY = 0;
-	       int menorX = 9999, menorY = 9999;
-	       for(int p=0; p < ArrayX.size(); p++) {
-	    	   if(ArrayX.get(p) > maiorX)
-	    		   maiorX= ArrayX.get(p);
-	    	   
-	    	   if(ArrayX.get(p) < menorX)
-	    		   menorX=ArrayX.get(p);
-	    	  // System.out.println(ArrayX[p]);
-	       }
-	       
-	       for(int l=0; l < ArrayY.size(); l++) {
-	    	   if(ArrayY.get(l) > maiorY)
-	    		   maiorY= ArrayY.get(l);
-	    	   
-	    	   if(ArrayX.get(l) < menorY)
-	    		   menorY = ArrayX.get(l);
-	    	   
-	    	   //System.out.println(ArrayY[l]);
-	       }
-	       
-	       for(int k=0; k < ObjObstacles.size(); k++) {
-	    	   if(ObjObstacles.get(k).x > maiorX) 
-	    		   maiorX = ObjObstacles.get(k).x;
-	    	   
-	    	   if(ObjObstacles.get(k).x < menorX)
-	    		   menorX = ObjObstacles.get(k).x;
-	    	   
-	    	   if(ObjObstacles.get(k).y > maiorY)
-	    		   maiorY = ObjObstacles.get(k).y;
-	    	   
-	    	   if(ObjObstacles.get(k).y < menorY)
-	    		   menorY = ObjObstacles.get(k).y;
-	       }
-	     
-			//System.out.println("MAIOR X" + maiorX + " MAior Y" + maiorY + " MEnor X" + menorX + " MenorY"+menorY);
-	        
-			maiorX += 2;
-			maiorY += 2;
-		    menorX -= 2; 
-		    menorY -= 2;
-			
-		    int dimensaoX = maiorX - menorX;
-		    int dimensaoY = maiorY - menorY;
-		    //System.out.println("Dim X" + dimensaoX + " Dim Y" + dimensaoY);
-		    
-		    final int tamanhoX = 800 / dimensaoX;
-		    final int tamanhoY = 600 / dimensaoY;
-		    //System.out.println("TAM X" + tamanhoX + " TAM Y" + tamanhoY);
-		    
-	        robot.setLayout(null);
-	        robot.setBounds((ObjRobot.getX()+1)*tamanhoX, (ObjRobot.getY()+1)*tamanhoY,tamanhoX, tamanhoY);
-	        
-	        
-	        for(int i=0; i< ObjBoxes.size(); i++){
-	        	JLabel newLabel = new JLabel(box);
-	        	newLabel.setLayout(null);
-	        	newLabel.setBounds((ObjBoxes.get(i).getX()+1)*tamanhoX, (ObjBoxes.get(i).getY()+1)*tamanhoY, tamanhoX, tamanhoY);
-	        	background.add(newLabel);
-	        }
-	        
-	        for(int i=0; i< ObjObstacles.size(); i++){
-	        	JLabel newLabel = new JLabel(obst);
-	        	newLabel.setLayout(null);
-	        	newLabel.setBounds((ObjObstacles.get(i).x+1)*tamanhoX, (ObjObstacles.get(i).y+1)*tamanhoY, tamanhoX, tamanhoY);
-	        	background.add(newLabel);
-	        }
-	        
-	        war2.setLayout(null);
-	        war2.setBounds((ObjWare.getX()+1)*tamanhoX, (ObjWare.getY()+1)*tamanhoY, tamanhoX, tamanhoY);
-	        
-	        background.add(robot);
-	        background.add(war2);
-	        
-	        //po teste
-	        path.add(p1);
-	        path.add(p2);
-	        path.add(p3);
-	        //path.add(p4);
-	        
-	        Timer timer = new Timer(500, new ActionListener() {
-	            public void actionPerformed(ActionEvent evt) {
-	            	
-	            	int i = vezes;
-	     	        vezes++;
-	     	        if(vezes == path.size()+1){
-	     	        	try {
-	     	        	   for(int j=0; j < path.size(); j++) {
-	     	  	        	JLabel newLabel = new JLabel(rasto1);
-	     	  	        	newLabel.setLayout(null);
-	     	  	        	newLabel.setBounds((path.get(j).x+1)*tamanhoX, (path.get(j).y+1)*tamanhoY, tamanhoX, tamanhoY);
-	     	  	        	background.add(newLabel);
-	     	        	   }
-							Thread.sleep(50000);
-						} catch (InterruptedException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-	     	        	System.exit(0);
-	     	        }
-	     	        
-	     	        //for(int i=0; i < path.size(); i++) {
-	     	        	int x = ObjRobot.getX();
-	     	        	int y = ObjRobot.getY();
-	     	        	
-	     	        	System.out.println("X- "+x + " Y " + y + "    " + "PX " + path.get(i).x + "  PY "+ path.get(i).y);
-	     	        	
-	     	        	//andar na hortizontal para postiva
-	     	        	if(path.get(i).x == x+1 && path.get(i).y == y){
-	     	        		robot.setBounds((path.get(i).x+1)*tamanhoX, (ObjRobot.getY()+1)*tamanhoY, tamanhoX, tamanhoY);
-	     		        	background.add(robot);
-	     		        	ObjRobot.setX(path.get(i).x);
-	     		        	System.out.println(ObjRobot.getX());
-	     		        	
-	     	        	}
-	     	        	//andar na vertical para postiiva
-	     	        	else if(path.get(i).y == y+1 && path.get(i).x == x) {
-	     	        		robot.setBounds((ObjRobot.getX()+1)*tamanhoX, (path.get(i).y+1)*tamanhoY, tamanhoX, tamanhoY);
-	     		        	background.add(robot);
-	     		        	ObjRobot.setY(path.get(i).y);
-	     		        
-	     	        	}
-	     	        	//andar na horizontal para negativo
-	     	        	else if(path.get(i).x == x-1 && path.get(i).y == y) {
-	     	        		robot.setBounds((path.get(i).x-1)*tamanhoX, (ObjRobot.getY()+1)*tamanhoY, tamanhoX, tamanhoY);
-	     		        	background.add(robot);
-	     		        	ObjRobot.setX(path.get(i).x);
-	     		        
-	     	        	}  
-	     	        	//andar na vertical para negativo
-	     	        	else if(path.get(i).y == y-1 && path.get(i).x == x) {
-	     	        		robot.setBounds((ObjRobot.getX()+1)*tamanhoX, (path.get(i).y-1)*tamanhoY, tamanhoX, tamanhoY);
-	     		        	background.add(robot);
-	     		        	ObjRobot.setY(path.get(i).y);
-	     		        
-	     	        	} 
-	     	        	//andar na diagonal ++
-	     	        	else if(path.get(i).y == y+1 && path.get(i).x == x+1) {
-	     	        		robot.setBounds((path.get(i).x+1)*tamanhoX, (path.get(i).y+1)*tamanhoY, tamanhoX, tamanhoY);
-	     		        	background.add(robot);
-	     		        	ObjRobot.setY(path.get(i).y);
-	     		        	ObjRobot.setX(path.get(i).x);
-	     		        
-	     	        	} 
-	     	        	//andar na diagonal --
-	     	        	else if(path.get(i).y == y-1 && path.get(i).x == x-1) {
-	     	        		robot.setBounds((path.get(i).x-1)*tamanhoX, (path.get(i).y-1)*tamanhoY, tamanhoX, tamanhoY);
-	     		        	background.add(robot);
-	     		        	ObjRobot.setY(path.get(i).y);
-	     		        	ObjRobot.setX(path.get(i).x);
-	     		       
-	     	        	} 
-	     	        	//andar na diagonal +-
-	     	        	else if(path.get(i).y == y-1 && path.get(i).x == x+1) {
-	     	        		robot.setBounds((path.get(i).x+1)*tamanhoX, (path.get(i).y-1)*tamanhoY, tamanhoX, tamanhoY);
-	     		        	background.add(robot);
-	     		        	ObjRobot.setY(path.get(i).y);
-	     		        	ObjRobot.setX(path.get(i).x);
-	     		        	
-	     	        	} 
-	     	        	//andar na diagonal -+
-	     	        	else if(path.get(i).y == y+1 && path.get(i).x == x-1) {
-	     	        		robot.setBounds((path.get(i).x-1)*tamanhoX, (path.get(i).y+1)*tamanhoY, tamanhoX, tamanhoY);
-	     		        	background.add(robot);
-	     		        	ObjRobot.setY(path.get(i).y);
-	     		        	ObjRobot.setX(path.get(i).x);
-	     		        	
-	     	        	} 
-	     	             	        	     	        	
-	     	        //}
-	     	        
-	                repaint();
-	            }
-	        });
-	        
-	       
-	        timer.setRepeats(true);
-	        timer.setCoalesce(true);
-	        timer.start();
-	        
-	        
+   
 	        pack();
 	        setLocationRelativeTo(null);
 	        setVisible(true);
 
 	    }
 	 
+	 public void Game(){}
+	 
+	 
 	 public class ExitButtonHandler implements ActionListener
 	    {
 	        public void actionPerformed(ActionEvent e)
 	        {
-	        	
-	            System.exit(0);
+	        	System.exit(0);
 
 	        }
 	    }
@@ -312,8 +131,198 @@ public class Interface extends JFrame{
 	    {
 	        public void actionPerformed(ActionEvent e)
 	        {
+	        	
+	        	List<Integer> ArrayX = new ArrayList<Integer>();
+				List<Integer> ArrayY = new ArrayList<Integer>();
+				
+				ArrayX.add(ObjRobot.getX());
+				ArrayY.add(ObjRobot.getY());
+				ArrayX.add(ObjWare.getX());
+				ArrayY.add(ObjWare.getY());
+				
+				//System.out.println("ROBOT: " + ObjRobot.getX() + ObjRobot.getY() + " ARMAZZEM: " + ObjWare.getX()+ ObjWare.getY());
+				
+		       for(int i =0; i < ObjBoxes.size(); i++) {
+		    	   ArrayX.add(ObjBoxes.get(i).getX());
+		    	   ArrayY.add(ObjBoxes.get(i).getY());
+		    	   //System.out.println(ObjBoxes.get(i).getX() + ObjBoxes.get(i).getY());
+		       }
+		       
+		       int maiorX = 0, maiorY = 0;
+		       int menorX = 9999, menorY = 9999;
+		       for(int p=0; p < ArrayX.size(); p++) {
+		    	   if(ArrayX.get(p) > maiorX)
+		    		   maiorX= ArrayX.get(p);
+		    	   
+		    	   if(ArrayX.get(p) < menorX)
+		    		   menorX=ArrayX.get(p);
+		    	  // System.out.println(ArrayX[p]);
+		       }
+		       
+		       for(int l=0; l < ArrayY.size(); l++) {
+		    	   if(ArrayY.get(l) > maiorY)
+		    		   maiorY= ArrayY.get(l);
+		    	   
+		    	   if(ArrayX.get(l) < menorY)
+		    		   menorY = ArrayX.get(l);
+		    	   
+		    	   //System.out.println(ArrayY[l]);
+		       }
+		       
+		       for(int k=0; k < ObjObstacles.size(); k++) {
+		    	   if(ObjObstacles.get(k).x > maiorX) 
+		    		   maiorX = ObjObstacles.get(k).x;
+		    	   
+		    	   if(ObjObstacles.get(k).x < menorX)
+		    		   menorX = ObjObstacles.get(k).x;
+		    	   
+		    	   if(ObjObstacles.get(k).y > maiorY)
+		    		   maiorY = ObjObstacles.get(k).y;
+		    	   
+		    	   if(ObjObstacles.get(k).y < menorY)
+		    		   menorY = ObjObstacles.get(k).y;
+		       }
+		     
+				//System.out.println("MAIOR X" + maiorX + " MAior Y" + maiorY + " MEnor X" + menorX + " MenorY"+menorY);
+		        
+				maiorX += 2;
+				maiorY += 2;
+			    menorX -= 2; 
+			    menorY -= 2;
+				
+			    int dimensaoX = maiorX - menorX;
+			    int dimensaoY = maiorY - menorY;
+			    //System.out.println("Dim X" + dimensaoX + " Dim Y" + dimensaoY);
+			    
+			    final int tamanhoX = 800 / dimensaoX;
+			    final int tamanhoY = 600 / dimensaoY;
+			    //System.out.println("TAM X" + tamanhoX + " TAM Y" + tamanhoY);
+			    
+		        robot.setLayout(null);
+		        robot.setBounds((ObjRobot.getX()+1)*tamanhoX, (ObjRobot.getY()+1)*tamanhoY,tamanhoX, tamanhoY);
+		        
+		        
+		        for(int i=0; i< ObjBoxes.size(); i++){
+		        	JLabel newLabel = new JLabel(box);
+		        	newLabel.setLayout(null);
+		        	newLabel.setBounds((ObjBoxes.get(i).getX()+1)*tamanhoX, (ObjBoxes.get(i).getY()+1)*tamanhoY, tamanhoX, tamanhoY);
+		        	background.add(newLabel);
+		        }
+		        
+		        for(int i=0; i< ObjObstacles.size(); i++){
+		        	JLabel newLabel = new JLabel(obst);
+		        	newLabel.setLayout(null);
+		        	newLabel.setBounds((ObjObstacles.get(i).x+1)*tamanhoX, (ObjObstacles.get(i).y+1)*tamanhoY, tamanhoX, tamanhoY);
+		        	background.add(newLabel);
+		        }
+		        
+		        war2.setLayout(null);
+		        war2.setBounds((ObjWare.getX()+1)*tamanhoX, (ObjWare.getY()+1)*tamanhoY, tamanhoX, tamanhoY);
+		        
+		        background.add(robot);
+		        background.add(war2);
+		        
+		        //po teste
+		        path.add(p1);
+		        path.add(p2);
+		        path.add(p3);
+		        //path.add(p4);
+		        
+		        Timer timer = new Timer(500, new ActionListener() {
+		            public void actionPerformed(ActionEvent evt) {
+		            	
+		            	int i = vezes;
+		     	        vezes++;
+		     	        if(vezes == path.size()+1){
+		     	        	   for(int j=0; j < path.size(); j++) {
+		     	  	        	JLabel newLabel = new JLabel(rasto1);
+		     	  	        	newLabel.setLayout(null);
+		     	  	        	newLabel.setBounds((path.get(j).x+1)*tamanhoX, (path.get(j).y+1)*tamanhoY, tamanhoX, tamanhoY);
+		     	  	        	background.add(newLabel);
+		     	        	   }
+		     	        	System.exit(0);
+		     	        }
+		     	        
+		     	        //for(int i=0; i < path.size(); i++) {
+		     	        	int x = ObjRobot.getX();
+		     	        	int y = ObjRobot.getY();
+		     	        	
+		     	        	System.out.println("X- "+x + " Y " + y + "    " + "PX " + path.get(i).x + "  PY "+ path.get(i).y);
+		     	        	
+		     	        	//andar na hortizontal para postiva
+		     	        	if(path.get(i).x == x+1 && path.get(i).y == y){
+		     	        		robot.setBounds((path.get(i).x+1)*tamanhoX, (ObjRobot.getY()+1)*tamanhoY, tamanhoX, tamanhoY);
+		     		        	background.add(robot);
+		     		        	ObjRobot.setX(path.get(i).x);
+		     		        	System.out.println(ObjRobot.getX());
+		     		        	
+		     	        	}
+		     	        	//andar na vertical para postiiva
+		     	        	else if(path.get(i).y == y+1 && path.get(i).x == x) {
+		     	        		robot.setBounds((ObjRobot.getX()+1)*tamanhoX, (path.get(i).y+1)*tamanhoY, tamanhoX, tamanhoY);
+		     		        	background.add(robot);
+		     		        	ObjRobot.setY(path.get(i).y);
+		     		        
+		     	        	}
+		     	        	//andar na horizontal para negativo
+		     	        	else if(path.get(i).x == x-1 && path.get(i).y == y) {
+		     	        		robot.setBounds((path.get(i).x-1)*tamanhoX, (ObjRobot.getY()+1)*tamanhoY, tamanhoX, tamanhoY);
+		     		        	background.add(robot);
+		     		        	ObjRobot.setX(path.get(i).x);
+		     		        
+		     	        	}  
+		     	        	//andar na vertical para negativo
+		     	        	else if(path.get(i).y == y-1 && path.get(i).x == x) {
+		     	        		robot.setBounds((ObjRobot.getX()+1)*tamanhoX, (path.get(i).y-1)*tamanhoY, tamanhoX, tamanhoY);
+		     		        	background.add(robot);
+		     		        	ObjRobot.setY(path.get(i).y);
+		     		        
+		     	        	} 
+		     	        	//andar na diagonal ++
+		     	        	else if(path.get(i).y == y+1 && path.get(i).x == x+1) {
+		     	        		robot.setBounds((path.get(i).x+1)*tamanhoX, (path.get(i).y+1)*tamanhoY, tamanhoX, tamanhoY);
+		     		        	background.add(robot);
+		     		        	ObjRobot.setY(path.get(i).y);
+		     		        	ObjRobot.setX(path.get(i).x);
+		     		        
+		     	        	} 
+		     	        	//andar na diagonal --
+		     	        	else if(path.get(i).y == y-1 && path.get(i).x == x-1) {
+		     	        		robot.setBounds((path.get(i).x-1)*tamanhoX, (path.get(i).y-1)*tamanhoY, tamanhoX, tamanhoY);
+		     		        	background.add(robot);
+		     		        	ObjRobot.setY(path.get(i).y);
+		     		        	ObjRobot.setX(path.get(i).x);
+		     		       
+		     	        	} 
+		     	        	//andar na diagonal +-
+		     	        	else if(path.get(i).y == y-1 && path.get(i).x == x+1) {
+		     	        		robot.setBounds((path.get(i).x+1)*tamanhoX, (path.get(i).y-1)*tamanhoY, tamanhoX, tamanhoY);
+		     		        	background.add(robot);
+		     		        	ObjRobot.setY(path.get(i).y);
+		     		        	ObjRobot.setX(path.get(i).x);
+		     		        	
+		     	        	} 
+		     	        	//andar na diagonal -+
+		     	        	else if(path.get(i).y == y+1 && path.get(i).x == x-1) {
+		     	        		robot.setBounds((path.get(i).x-1)*tamanhoX, (path.get(i).y+1)*tamanhoY, tamanhoX, tamanhoY);
+		     		        	background.add(robot);
+		     		        	ObjRobot.setY(path.get(i).y);
+		     		        	ObjRobot.setX(path.get(i).x);
+		     		        	
+		     	        	} 
+		     	             	        	     	        	
+		     	        //}
+		     	        
+		                repaint();
+		            }
+		        });
+		        
+		       
+		        timer.setRepeats(true);
+		        timer.setCoalesce(true);
+		        timer.start();
 
-	            System.exit(0);
+	
 
 	        }
 	    }
